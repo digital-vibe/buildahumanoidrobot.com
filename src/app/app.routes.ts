e8e6f9460fd8fa7be5912551827f19f8f01f2314
Coen Mooij <coenmooij@gmail.com>
2024-02-19 20:29:43 +0100

    setup homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomePage } from './pages/home';
+export const routes: Routes = [
+  {path: '', pathMatch: 'full', component: HomePage},
+];
